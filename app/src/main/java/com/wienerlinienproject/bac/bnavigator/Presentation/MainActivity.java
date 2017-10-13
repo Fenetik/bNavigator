928ed975b3e7835a29364139dfedd3d542591a1e
Fenetik <L.Frank1@gmx.at>
2017-10-14 01:22:19 +0200

    Refactoring Added Services Added Callback and Boradcastreceiver

---
+++ app/src/main/java/com/wienerlinienproject/bac/bnavigator/Presentation/MainActivity.java
@@ -0,0 +1,154 @@
+package  com.wienerlinienproject.bac.bnavigator.Presentation;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.estimote.coresdk.common.config.EstimoteSDK;
+import com.wienerlinienproject.bac.bnavigator.R;
+import com.wienerlinienproject.bac.bnavigator.Service.BeaconService;
+//import com.wienerlinienproject.bac.bnavigator.Service.CloudService;
+
+
+public class MainActivity extends AppCompatActivity implements ServiceConnection {
+
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+
+    private BeaconService beaconService;
+    //private CloudService cloudService;
+    private ServiceCallbackReceiver callbackReceiver = new ServiceCallbackReceiver();
+    boolean beaconServiceIsBound = false;
+    private TextView beaconLog;
+
+    //TODO in diesem Fall kann man nur eine Serviceconnection haben?
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //hier sollen nur sachen rein die die UI benötigt
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        beaconLog = (TextView) findViewById(R.id.beaconLog);
+        beaconLog.setMovementMethod(new ScrollingMovementMethod());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Android M Permission check
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("This app needs location access.");
+                builder.setMessage("Please grant location access so this app can detect beacons.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener(){
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                    }
+                });
+                builder.show();
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        //hier sachen rein die unabhängig von der UI geladen werden können
+        super.onStart();
+
+        // AppId and AppToken von der developer.estimote-website
+        EstimoteSDK.initialize(getApplicationContext(), "natasa-nikic-info-s-your-o-e6g", "b437d62cd736bece0f9a475fe861e3d4");
+
+        Intent intentBeaconService = new Intent(this, BeaconService.class);
+        bindService(intentBeaconService, this, Context.BIND_AUTO_CREATE);
+
+        //TODO
+        //Intent intentCloudService = new Intent (this, CloudService.class);
+       //bindService(intentCloudService, this, Context.BIND_AUTO_CREATE);
+
+        //filtern worauf der receiver hören soll (Anroid vrwendet genrel broadcast)
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ServiceCallbackReceiver.BROADCAST_BeaconService);
+        filter.addAction(ServiceCallbackReceiver.BROADCAST_cloudService);
+        registerReceiver(callbackReceiver, filter);
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+        BeaconService.MyBinder binder = (BeaconService.MyBinder) iBinder;
+        beaconService = binder.getService();
+        beaconServiceIsBound = true;
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName componentName) {
+        beaconService = null;
+        beaconServiceIsBound = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(this);
+        unregisterReceiver(callbackReceiver);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[],
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("BeaconInit", "coarse location permission granted");
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+                    });
+                    builder.show();
+                }
+
+                return;
+            }
+        }
+    }
+
+
+    public class ServiceCallbackReceiver extends BroadcastReceiver {
+
+        public static final String BROADCAST_BeaconService ="com.wienerlinienproject.bac.bnavigator.beaconServiceAction";
+        public static final String BROADCAST_cloudService ="com.wienerlinienproject.bac.bnavigator.cloudServiceAction";
+
+        public static final String BROADCAST_PARAM = "param";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("ServiceCallbackReceiver", "received callback");
+
+            if(intent.getAction().equals(BROADCAST_BeaconService)){
+                Log.d("ServiceCallbackReceiver", "got " + intent.getStringExtra(BROADCAST_PARAM));
+
+                beaconLog.append("\n"+intent.getStringExtra(BROADCAST_PARAM));
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/wienerlinienproject/bac/bnavigator/Service/BeaconService.java
@@ -0,0 +1,165 @@
+package  com.wienerlinienproject.bac.bnavigator.Service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.recognition.packets.Beacon;
+import com.estimote.coresdk.service.BeaconManager;
+import com.wienerlinienproject.bac.bnavigator.Presentation.MainActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BeaconService extends Service {
+
+
+    private  Map<String, List<String>> PLACES_BY_BEACONS;
+    private BeaconManager beaconManager;
+    private BeaconRegion region;
+    private String destination = "Mc Donalds";
+
+    private final IBinder mBinder = new MyBinder();
+
+    public BeaconService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        beaconManager = new BeaconManager(getApplicationContext());
+
+        region = new BeaconRegion("ranged region", null, null, null);
+
+        Log.d("moniiii","setting Listener");
+        beaconManager.setRangingListener(new BeaconManager.BeaconRangingListener() {
+            @Override
+            public void onBeaconsDiscovered(BeaconRegion region, List<Beacon> list) {
+                if (!list.isEmpty()) {
+                    Beacon nearestBeacon = list.get(0);
+                    List<String> places = placesNearBeacon(nearestBeacon);
+                    String output = "";
+                    if(!places.isEmpty()){
+                        //wenn nur 1 place in der Liste ist
+                        if(places.size() == 1){
+                            output += places.get(0) + " (Nothing to navigate.)";
+                            Log.d("RangingListener", places.get(0));
+                        }else{
+                            output += places.get(0) + " "+navigate(nearestBeacon, destination);
+                            Log.d("RangingListener", places.get(0));
+                        }
+
+                    }else {
+                        output+= "No new places";
+                        Log.d("RangingListener", "No new places");
+                    }
+
+                    Intent broadcast = new Intent(MainActivity.ServiceCallbackReceiver.BROADCAST_BeaconService);
+                    broadcast.putExtra(MainActivity.ServiceCallbackReceiver.BROADCAST_PARAM, output);
+                    sendBroadcast(broadcast);
+                    Log.d("moni","sending msg:" +output);
+
+                }else{
+                    //dafuq
+                }
+
+                /*String s = "";
+                for(Beacon temp : list){
+                    s += "Beacons found:" + temp.getProximityUUID().toString() + "\n";
+                }
+                Log.d("monii","beacon discovered");
+                String temp ="found one";
+
+                Intent broadcast = new Intent(MainActivity.ServiceCallbackReceiver.BROADCAST_BeaconService);
+                broadcast.putExtra(MainActivity.ServiceCallbackReceiver.BROADCAST_PARAM, s);
+                sendBroadcast(broadcast);
+                Log.d("moni","sending msg:" +temp);*/
+            }
+        });
+
+        Map<String, List<String>> placesByBeacons = new HashMap<>();
+        placesByBeacons.put("3:2", new ArrayList<String>() {{
+            //closest
+            add("Mc Donalds");
+            //second closest
+            add("Starbucks");
+            //furthest away
+            add("Subway");
+        }});
+        placesByBeacons.put("2:2", new ArrayList<String>() {{
+            add("Subway");
+            add("Starbucks");
+            add("Mc Donalds");
+        }});
+
+        placesByBeacons.put("4:4", new ArrayList<String>() {{
+            add("Burgerking");
+        }});
+
+        placesByBeacons.put("1:1", new ArrayList<String>() {{
+            add("Starbucks");
+            add("Subway");
+            add("Mc Donalds");
+        }});
+        PLACES_BY_BEACONS = Collections.unmodifiableMap(placesByBeacons);
+
+
+    }
+
+    private void run(){
+        Log.d("BeaconService", "run");
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override public void onServiceReady() {
+                beaconManager.startRanging(region);
+                Log.d("ranging","started");
+            }
+        });
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        run();
+        return mBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        beaconManager.disconnect();
+
+        //Service wird sicher beendet sobald sich jemand unbindet
+        this.stopSelf();
+
+        return super.onUnbind(intent);
+    }
+
+    private List<String> placesNearBeacon(Beacon beacon) {
+        String beaconKey = String.format("%d:%d", beacon.getMajor(), beacon.getMinor());
+        if (PLACES_BY_BEACONS.containsKey(beaconKey)) {
+            return PLACES_BY_BEACONS.get(beaconKey);
+        }
+        return Collections.emptyList();
+    }
+
+    private String navigate(Beacon beacon, String destination){
+
+        if (destination.equals(placesNearBeacon(beacon).get(0)))
+            return "Arrived at destination";
+        return "Go to: "+placesNearBeacon(beacon).get(1);
+    }
+
+   public class MyBinder extends Binder {
+
+       public BeaconService getService(){
+            return BeaconService.this;
+        }
+    }
+
+}
