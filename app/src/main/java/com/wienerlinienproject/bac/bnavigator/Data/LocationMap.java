ca05fb1fc6b7fcfc2d63a999c6f652b7c50102ef
Fenetik <L.Frank1@gmx.at>
2018-03-04 16:16:44 +0100

    merged with drawLocations (without beaconservice)

--- app/src/main/java/com/wienerlinienproject/bac/bnavigator/Data/LocationMap.java
+++ app/src/main/java/com/wienerlinienproject/bac/bnavigator/Data/LocationMap.java
@@ -1,19 +1,42 @@
 package com.wienerlinienproject.bac.bnavigator.Data;
 
-
-
 import java.util.HashMap;
 import java.util.Map;
 
-
-//Stores all Location Objects (the whole facility)
+//TODO wo wirklich intitialisieren? ws beaconservice => mittels broadcast an mainactivity schicken?
+//Stores all Location Objects
 public class LocationMap {
+
     //die Location in der sich der User gerade befindet
     private String activeLocation;
     //Key should be the locationName stored in the Cloud
-    private Map<String,LocationObject> locations = new HashMap();
+    private Map<String,LocationObject> locations;
 
     public LocationMap(){
-        //TODO intitialize LocationObjects and write to Map (wo:beaconservice)
+        locations = new HashMap<>();
+    }
+
+    public void addLocation(String name, LocationObject locationObject){
+
+        locations.put(name, locationObject);
+    }
+    public String getActiveLocation() {
+        return activeLocation;
+    }
+
+    public void setActiveLocation(String activeLocation) {
+        this.activeLocation = activeLocation;
+    }
+
+    public Map<String, LocationObject> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(Map<String, LocationObject> locations) {
+        this.locations = locations;
+    }
+
+    public LocationObject getLocationByName(String name){
+        return locations.get(name);
     }
 }
